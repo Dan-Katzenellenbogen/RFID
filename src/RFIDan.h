dc45bc9c35b7b62354edd660152917f7df9ed1e2
Dan-Katzenellenbogen <150925149+Dan-Katzenellenbogen@users.noreply.github.com>
2025-05-29 08:57:28 +0300

    Update RFIDan.h

--- src/RFIDan.h
+++ src/RFIDan.h
@@ -12,7 +12,7 @@
   public: 
    RFIDan(int RFID_pin);
    void begin();
-   void check(String Tag1, String Tag2, String Name);
+   void check(String Tag1, String Tag2, String Tag3, String Name);
    void printlcd(bool access, int ID);
    void neo(int pixno, int r, int g, int b);
    void allpix(int r, int g, int b);
